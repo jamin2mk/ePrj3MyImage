751116869bc013a1fec75f9e649fa00b75f80b07
Kim Quang Khúc <kira.retribution@gmail.com>
2019-01-22 15:18:26 +0700

    new init

--- MyImage/MyImage/MyImageService/IServiceMI.cs
+++ MyImage/MyImage/MyImageService/IServiceMI.cs
@@ -5,14 +5,15 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
+using MIClient.Models;
 
-
-namespace WebService
+namespace MyImageService
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
     [ServiceContract]
     public interface IServiceMI
     {
+
         [OperationContract]
         bool Create(tb_customer cust);
     }
